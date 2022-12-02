58a75dba1338ea6d4b83879a76862139de14a4cd
alxbckr <becker.alexander.82@gmail.com>
2022-12-02 20:24:06 +0100

    Day1 done

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # aoc22
 AdventOfCode v22
+
+How to run typescript files:
+
+npm install -g typescript
+npm install -g ts-node
+ts-node typescript-file.ts
---
+++ day1/solution1.ts
@@ -0,0 +1,35 @@
+import * as fs from 'fs';
+
+function read(filename: string): string[] {
+    let content: string;
+    let res: string[] = [];
+    try{
+        content = fs.readFileSync(`${process.cwd()}/${filename}`,'utf-8');
+    } catch(e){
+        console.log(e);
+        process.exit(9);
+    }
+    content.split(/\r?\n/).forEach(line =>  {
+            res.push(line);
+        });
+    return res;
+}
+
+function solve(content: string[]){
+    let sum = 0;
+    let max = 0;
+    content.forEach(line => {
+        if (line === "") {
+            sum = 0;
+            return;
+        }
+        sum += parseInt(line);
+        if (sum > max) {
+            max = sum;
+        }
+    })
+    console.log(max);
+}
+
+const content = read(process.argv.slice(2)[0]);
+solve(content);
