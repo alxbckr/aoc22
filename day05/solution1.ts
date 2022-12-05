ac374d213113b453346331e93d310ca799b1e8d8
Alexander Becker <alexander.becker02@sap.com>
2022-12-05 08:29:55 +0100

    Day 05

---
+++ day05/solution1.ts
@@ -0,0 +1,46 @@
+import * as fs from 'fs';
+
+function read(filename: string): string[] {
+    let content: string;
+    let res: string[] = [];
+    try{
+        content = fs.readFileSync(`${process.cwd()}/${filename}`,'utf-8');
+    } catch(e){
+        console.log(e);
+        process.exit(9);
+    }
+    content.split(/\r?\n/).forEach(line =>  {
+            res.push(line);
+        });
+    return res;
+}
+
+function solve(content: string[]){    
+    let towers: string[][] = []
+    let towersIdx = content[content.indexOf('')-1].match(/\d+/g);
+    if (!towersIdx) return;
+    towersIdx.forEach(t => {towers.push([])});
+
+    for(let i = content.indexOf('')-2;i>=0;i--){
+        let p = content[i].match(/.{1,4}/g);
+        if (!p) continue;
+        for (let j = 0; j<towers.length;j++){
+            if (p[j][1] != ' ') towers[j].push(p[j][1]);
+        }
+    }
+
+    for(let i: number = content.indexOf('')+1;i<content.length;i++){
+        //move N from x to y
+        let instr = content[i].match(/\d+/g);
+        if (!instr) return;
+        for(let j = 0; j < parseInt(instr[0]);j++){
+            towers[parseInt(instr[2])-1].push(towers[parseInt(instr[1])-1].pop()!);
+        }
+    }    
+    let answer = '';
+    towers.forEach(t=>{answer += t[t.length-1]});
+    console.log(answer);
+}
+
+const content = read(process.argv.slice(2)[0]);
+solve(content);
