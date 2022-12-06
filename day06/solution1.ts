bc236503cf66cfe0c64bafe51fcc5c494f8bcfce
alxbckr <becker.alexander.82@gmail.com>
2022-12-06 07:04:07 +0100

    Day 06

---
+++ day06/solution1.ts
@@ -0,0 +1,40 @@
+import * as fs from 'fs';
+
+function read(filename: string): string[] {
+    let content: string;
+    let res: string[] = [];
+    try{
+        content = fs.readFileSync(`${process.cwd()}/${filename}`,'utf-8');
+    } catch(e){
+        console.log(e);
+        process.exit(9);
+    }
+    content.split(/\r?\n/).forEach(line =>  {
+            res.push(line);
+        });
+    return res;
+}
+
+function solve(content: string[]){    
+    content.forEach(c=>{
+        let b: string[] = [];
+        let idx = 0
+        c.split('').every((ch,i)=>{
+            if (b.length<=3) {
+                b.push(ch);
+                return true;
+            }
+            if (b.every((m,i)=>{return b.indexOf(m) === i;})){
+                idx = i;
+                return false;
+            }            
+            b.shift();
+            b.push(ch);
+            return true;
+        }); 
+        console.log(idx);
+    });
+}
+
+const content = read(process.argv.slice(2)[0]);
+solve(content);
