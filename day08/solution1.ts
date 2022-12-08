a38cb3581b159efdcf3efa265a103e2797c99941
Alexander Becker <alexander.becker02@sap.com>
2022-12-08 11:22:04 +0100

    Day 08

---
+++ day08/solution1.ts
@@ -0,0 +1,66 @@
+import * as fs from 'fs';
+
+function read(filename: string): string[] {
+    let content: string;
+    let res: string[] = [];
+    try{
+        content = fs.readFileSync(`${process.cwd()}/${filename}`,'utf-8');
+    } catch(e){
+        console.log(e);
+        process.exit(9);
+    }
+    content.split(/\r?\n/).forEach(line =>  {
+            res.push(line);
+        });
+    return res;
+}
+
+type MapType = { 
+    [id: string]: string; 
+}
+
+function solve(content: string[]){    
+    let trees: number[][] = [];
+    let visible: number[][] = [];
+
+    content.forEach((c,i)=>{
+        let line = c.split('').map(Number);
+        let v: number[] = [];
+        line.forEach((m,j)=>{ if (i===0 || j === 0 || i === content.length-1 || j === line.length - 1) v.push(1); else v.push(0);});
+        trees.push(line);
+        visible.push(v);
+    });
+
+    let answer = 0;
+
+    // row visibility
+    for (let i = 1; i < trees.length-1;i++){
+        let max = trees[i][0];
+        for (let j = 1; j < trees[i].length-1;j++){            
+            if (trees[i][j] > max) { visible[i][j] = 1; max = trees[i][j]; } 
+        }
+        max = trees[i][trees.length-1];
+        for (let j = trees[i].length-2; j > 0;j--){
+            if (trees[i][j] > max) { visible[i][j] = 1; max = trees[i][j] } 
+        }
+    }
+
+    // column visibility
+    for (let j = 1; j < trees[0].length-1;j++) {
+        let max = trees[0][j];
+        for (let i = 1; i < trees.length-1;i++){
+            if (trees[i][j] > max) { visible[i][j] = 1; max = trees[i][j] }  
+        }
+        max = trees[trees.length-1][j];
+        for (let i = trees.length-2; i > 0 ;i--){
+            if (trees[i][j] > max) { visible[i][j] = 1; max = trees[i][j] }  
+        }
+    }    
+    visible.forEach(v=>{
+        v.forEach(k=>{if (k>0) answer++});
+    });
+    console.log(answer);
+}
+
+const content = read(process.argv.slice(2)[0]);
+solve(content);
---
+++ day08/solution2.ts
@@ -0,0 +1,78 @@
+import * as fs from 'fs';
+
+function read(filename: string): string[] {
+    let content: string;
+    let res: string[] = [];
+    try{
+        content = fs.readFileSync(`${process.cwd()}/${filename}`,'utf-8');
+    } catch(e){
+        console.log(e);
+        process.exit(9);
+    }
+    content.split(/\r?\n/).forEach(line =>  {
+            res.push(line);
+        });
+    return res;
+}
+
+type MapType = { 
+    [id: string]: string; 
+}
+
+function solve(content: string[]){    
+    let trees: number[][] = [];
+    let visible: number[][] = [];
+
+    content.forEach((c,i)=>{
+        let line = c.split('').map(Number);
+        let v: number[] = [];
+        line.forEach((m,j)=>{ if (i===0 || j === 0 || i === content.length-1 || j === line.length - 1) v.push(0); else v.push(1);});
+        trees.push(line);
+        visible.push(v);
+    });
+
+    let answer = 0;
+
+    //row visibility
+    for (let i = 1; i < trees.length-1;i++){        
+        for (let j = 1; j < trees[i].length-1;j++){            
+            let distance = 0;
+            for (let k = j-1; k >= 0;k--){
+                distance++; 
+                if (trees[i][k] >= trees[i][j]) break;
+            } 
+            visible[i][j] *= distance === 0 ? 1 : distance;
+            distance = 0;
+            for (let k = j+1; k <= trees[i].length-1;k++){
+                distance++; 
+                if (trees[i][k] >= trees[i][j]) break;
+            } 
+            visible[i][j] *= distance === 0 ? 1 : distance;
+        }
+    }
+
+    //column visibility
+    for (let j = 1; j < trees[0].length-1;j++) {
+        for (let i = 1; i < trees.length-1;i++){
+            let distance = 0;
+            for (let k = i-1; k >= 0;k--) {
+                distance++; 
+                if (trees[k][j] >= trees[i][j]) break;
+            } 
+            visible[i][j] *= distance === 0 ? 1 : distance;
+            distance = 0;
+            for (let k = i+1; k <= trees.length-1;k++){
+                distance++; 
+                if (trees[k][j] >= trees[i][j]) break;
+            } 
+            visible[i][j] *= distance === 0 ? 1 : distance;
+        }
+    }    
+    visible.forEach(v=>{
+        v.forEach(k=>{if (k>answer) answer=k});
+    });
+    console.log(answer);
+}
+
+const content = read(process.argv.slice(2)[0]);
+solve(content);
