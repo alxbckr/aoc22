1be3d7652f811b7e17e150e63930b35b323c8637
Alexander Becker <alexander.becker02@sap.com>
2022-12-14 16:56:39 +0100

    Temp

--- day12/solution1.ts
+++ day12/solution1.ts
@@ -20,14 +20,42 @@ type Graph = {
     adj: Array<number>[];    
 }
 
+function coord2Num(x : number, y : number, len : number) : number {
+    return y * len + x;
+}
+
 function solve(content: string[]){    
-
+    
+    let elems: Array<number>[] = [];
     let graph: Graph = {adj : []};
-
     let visited: boolean[] = [];
-    for (let i = 0; i < content.length; i++){
-        visited.push(false);
+    let len : number = content[0].length;
+    let start = 0;
+    let end = 0;
+
+    content.forEach((c)=>{
+        let row: number[] = [];
+        c.split("").forEach((el)=>{
+            row.push(el.charCodeAt(0));
+            visited.push(false);
+            graph.adj.push([]);
+        });
+        elems.push(row);
+    });
+
+    for (let y = 0; y < elems.length; y++){
+        for (let x = 0; x < len; x++) {
+            if (elems[y][x] === "S".charCodeAt(0)) start = coord2Num(x,y,len);
+            if (elems[y][x] === "E".charCodeAt(0)) end = coord2Num(x,y,len);
+            if (x < len-1 && Math.abs(elems[y][x+1] - elems[y][x]) === 1 ) graph.adj[coord2Num(x,y,len)].push(coord2Num(x+1,y,len));
+            if (x > 0 && Math.abs(elems[y][x-1] - elems[y][x]) === 1) graph.adj[coord2Num(x,y,len)].push(coord2Num(x-1,y,len));
+            if (y < content.length - 1 && Math.abs(elems[y+1][x] - elems[y][x]) === 1) graph.adj[coord2Num(x,y,len)].push(coord2Num(x,y+1,len));
+            if (y > 0 && Math.abs(elems[y-1][x] - elems[y][x]) === 1) graph.adj[coord2Num(x,y,len)].push(coord2Num(x,y-1,len));
+        }
     }
+
+    console.log(start, end);
+    console.log(graph);
 
     let q : number[] = [];
     visited[0] = true;
