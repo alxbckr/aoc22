31929c47337867a8f7fdfe763aba4879cbd4f8d5
alxbckr <becker.alexander.82@gmail.com>
2022-12-25 17:41:46 +0100

    day 12

--- day12/solution1.ts
+++ day12/solution1.ts
@@ -18,16 +18,37 @@ function read(filename: string): string[] {
 
 type Graph = {
     adj: Array<number>[];    
+    dist: Array<number>;    
 }
 
 function coord2Num(x : number, y : number, len : number) : number {
     return y * len + x;
 }
 
+function num2Coord(i: number, len : number) {
+    return {x : i % len, y: Math.floor(i / len)};
+}
+
+function printGraph(visited: boolean[], len : number, elems: Array<number>[]){    
+    let line = "";
+    let prevC = num2Coord(0, len);
+    for (let i = 0; i < visited.length;i++){
+        let c = num2Coord(i,len);
+        if (c.y !== prevC.y) {
+            console.log(line);
+            line = "";
+        }
+        if (visited[i]) line += String.fromCharCode(elems[c.y][c.x])
+        else line += "."         
+        prevC = c
+    }
+    console.log(line);
+}
+
 function solve(content: string[]){    
     
     let elems: Array<number>[] = [];
-    let graph: Graph = {adj : []};
+    let graph: Graph = {adj : [], dist : []};
     let visited: boolean[] = [];
     let len : number = content[0].length;
     let start = 0;
@@ -39,37 +60,52 @@ function solve(content: string[]){
             row.push(el.charCodeAt(0));
             visited.push(false);
             graph.adj.push([]);
+            graph.dist.push(0);
         });
         elems.push(row);
     });
 
     for (let y = 0; y < elems.length; y++){
         for (let x = 0; x < len; x++) {
-            if (elems[y][x] === "S".charCodeAt(0)) start = coord2Num(x,y,len);
-            if (elems[y][x] === "E".charCodeAt(0)) end = coord2Num(x,y,len);
-            if (x < len-1 && Math.abs(elems[y][x+1] - elems[y][x]) === 1 ) graph.adj[coord2Num(x,y,len)].push(coord2Num(x+1,y,len));
-            if (x > 0 && Math.abs(elems[y][x-1] - elems[y][x]) === 1) graph.adj[coord2Num(x,y,len)].push(coord2Num(x-1,y,len));
-            if (y < content.length - 1 && Math.abs(elems[y+1][x] - elems[y][x]) === 1) graph.adj[coord2Num(x,y,len)].push(coord2Num(x,y+1,len));
-            if (y > 0 && Math.abs(elems[y-1][x] - elems[y][x]) === 1) graph.adj[coord2Num(x,y,len)].push(coord2Num(x,y-1,len));
+            if (elems[y][x] === "S".charCodeAt(0)) {
+                elems[y][x] = "a".charCodeAt(0) - 1;
+                start = coord2Num(x,y,len);
+            }
+            if (elems[y][x] === "E".charCodeAt(0)) {
+                end = coord2Num(x,y,len);
+                elems[y][x] = "z".charCodeAt(0) + 1;
+            }
+        }
+    }
+
+    for (let y = 0; y < elems.length; y++){
+        for (let x = 0; x < len; x++) {
+            if (x < len-1 && elems[y][x+1] - elems[y][x] <= 1) graph.adj[coord2Num(x,y,len)].push(coord2Num(x+1,y,len));
+            if (x > 0 && elems[y][x-1] - elems[y][x] <=1) graph.adj[coord2Num(x,y,len)].push(coord2Num(x-1,y,len));
+            if (y < content.length - 1 && elems[y+1][x] - elems[y][x] <= 1) graph.adj[coord2Num(x,y,len)].push(coord2Num(x,y+1,len));
+            if (y > 0 && elems[y-1][x] - elems[y][x] <= 1) graph.adj[coord2Num(x,y,len)].push(coord2Num(x,y-1,len));
         }
     }
 
     console.log(start, end);
-    console.log(graph);
 
+    let distance = 0;
     let q : number[] = [];
-    visited[0] = true;
-    q.push(0);
+    visited[start] = true;
+    q.push(start);
     while(q.length > 0){
         const v = q.shift();
-        if (!v) break;
+        if (v === undefined || v === end) break;
         graph.adj[v].forEach(a=>{
             if (!visited[a]){
                 visited[a] = true;
                 q.push(a);
+                graph.dist[a] = graph.dist[v] + 1;
             }
-        });
+        });           
     }
+    printGraph(visited, len, elems);
+    console.log(graph.dist[end]);
 }
 
 const content = read(process.argv.slice(2)[0]);
--- day12/solution2.ts
+++ day12/solution2.ts
@@ -1,6 +1,5 @@
+import { group } from 'console';
 import * as fs from 'fs';
-import { monitorEventLoopDelay } from 'perf_hooks';
-import { StringifyOptions } from 'querystring';
 
 function read(filename: string): string[] {
     let content: string;
@@ -17,90 +16,92 @@ function read(filename: string): string[] {
     return res;
 }
 
-type Monkey = {
-    items : number[];
-    operation : string;
-    arg1: string;
-    arg2: string;
-    test: number;
-    monkeyToTrue: number;
-    monkeyToFalse: number;
-    countItems: number;
-};
-
-function newMonkey() : Monkey {
-    let m : Monkey = {items: [], operation: "", arg1: "", arg2: "", test: Number(0), monkeyToTrue:0, monkeyToFalse:0, countItems: 0};
-    return m;
+type Graph = {
+    adj: Array<number>[];    
 }
 
-function calcWorry(i : number, op: string, arg1: string, arg2: string, scale: number) : number{
-    let x : number = 0, y : number = 0; 
-    switch (arg1) {
-        case "old": x = i; break;
-        default: x = Number(arg1);
+function coord2Num(x : number, y : number, len : number) : number {
+    return y * len + x;
+}
+
+function calcMinDist(start: number, end : number, graph: Graph): number{
+    let dist: Array<number> = [];    
+    let visited: boolean[] = [];
+    let q : number[] = [];
+
+    graph.adj.forEach(()=>{
+        visited.push(false);
+        dist.push(0);
+    })
+
+    visited[start] = true;
+    q.push(start);
+    while(q.length > 0){
+        const v = q.shift();
+        if (v === undefined || v === end) break;
+        graph.adj[v].forEach(a=>{
+            if (!visited[a]){
+                visited[a] = true;
+                q.push(a);
+                dist[a] = dist[v] + 1;
+            }
+        });           
     }
-    switch (arg2) {
-        case "old": y = i; break;
-        default: y = Number(arg2);
-    }    
-    switch(op){
-        case '+': return (x + y)%scale;
-        case '-': return (x - y)%scale;
-        case '*': return (x * y)%scale;
-        case '/': return (x / y)%scale;
-    }
-    return 0;
+    return dist[end];    
 }
 
 function solve(content: string[]){    
-    let monkeys : Monkey[] = [];
-    let monkey = newMonkey();
-    content.forEach((c,i)=>{
-        switch (i % 7){
-            case 0: return; 
-            case 1: 
-                monkey.items = c.substring(18).split(',').map(Number); break;
-            case 2: 
-                [monkey.arg1, monkey.operation, monkey.arg2] = c.substring(19).split(" ");
-                break;
-            case 3: 
-                monkey.test = Number(c.substring(21)); 
-                break;
-            case 4: 
-                monkey.monkeyToTrue = Number(c.substring(29)); 
-                break;
-            case 5: 
-                monkey.monkeyToFalse = Number(c.substring(30)); 
-                break;
-            case 6: 
-                monkeys.push(monkey);
-                monkey = newMonkey(); 
-                return;
-        }
-    });
-    monkeys.push(monkey);
     
-    let scale = 1;
-    monkeys.forEach(m=>{scale*=m.test});
-    console.log(scale);
+    let elems: Array<number>[] = [];
+    let graph: Graph = {adj : []};
+    let len : number = content[0].length;
+    let start = 0;
+    let end = 0;
 
-    for (let round = 0; round < 10000; round++){
-        monkeys.forEach((m,mi)=>{
-            m.items.forEach(i=>{
-                m.countItems++;
-                let worry = calcWorry(i, m.operation, m.arg1, m.arg2, scale);
-                if (worry % m.test === 0) monkeys[m.monkeyToTrue].items.push(worry);
-                else monkeys[m.monkeyToFalse].items.push(worry);                 
-            });
-            m.items = [];
-        })
-        if ((round + 1) % 1000 === 0){
-            console.log(round);
-            monkeys.forEach((m,i)=>console.log("Monkey", i, "inspected items", m.countItems, "times."));
+    content.forEach((c)=>{
+        let row: number[] = [];
+        c.split("").forEach((el)=>{
+            row.push(el.charCodeAt(0));
+            graph.adj.push([]);
+        });
+        elems.push(row);
+    });
+
+    for (let y = 0; y < elems.length; y++){
+        for (let x = 0; x < len; x++) {
+            if (elems[y][x] === "S".charCodeAt(0)) {
+                elems[y][x] = "a".charCodeAt(0) - 1;
+                start = coord2Num(x,y,len);
+            }
+            if (elems[y][x] === "E".charCodeAt(0)) {
+                end = coord2Num(x,y,len);
+                elems[y][x] = "z".charCodeAt(0) + 1;
+            }
         }
-    };
-    monkeys.sort((a,b)=>{return b.countItems - a.countItems});
-    console.log(monkeys[0].countItems * monkeys[1].countItems);
+    }
+
+    for (let y = 0; y < elems.length; y++){
+        for (let x = 0; x < len; x++) {
+            if (x < len-1 && elems[y][x+1] - elems[y][x] <= 1) graph.adj[coord2Num(x,y,len)].push(coord2Num(x+1,y,len));
+            if (x > 0 && elems[y][x-1] - elems[y][x] <=1) graph.adj[coord2Num(x,y,len)].push(coord2Num(x-1,y,len));
+            if (y < content.length - 1 && elems[y+1][x] - elems[y][x] <= 1) graph.adj[coord2Num(x,y,len)].push(coord2Num(x,y+1,len));
+            if (y > 0 && elems[y-1][x] - elems[y][x] <= 1) graph.adj[coord2Num(x,y,len)].push(coord2Num(x,y-1,len));
+        }
+    }
+    console.log(start, end);
+
+    let minDist = Number.MAX_SAFE_INTEGER;
+    let paths = 0;
+    for (let y = 0; y < elems.length; y++){
+        for (let x = 0; x < len; x++) {
+            if (elems[y][x] === "a".charCodeAt(0)){
+                const d = calcMinDist(coord2Num(x,y,len),end,graph);
+                if (d < minDist && d > 0) minDist = d;
+                paths++;
+            }            
+        }
+    }            
+    console.log(minDist, paths);
 }
 
 const content = read(process.argv.slice(2)[0]);
